a8b80edffae7d008b92e8c4d6fca5d763f7073be
Mohsin0582 <m.mohsin.shahzad0582@gmail.com>
2022-08-13 08:26:48 +0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Minor Changes
         </a>
       </header>
     </div>
